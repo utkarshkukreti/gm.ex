e00525897ebc40de69d5e90cec6c653a9be7ebf4
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2016-03-24 15:09:45 +0530

    feat(Gm.write/2)

--- lib/gm.ex
+++ lib/gm.ex
@@ -6,4 +6,14 @@ defmodule Gm do
   def open(path) when is_binary(path) do
     %Command{args: [path]}
   end
+
+  def write(%Command{args: args}, path) when is_binary(path) do
+    command = "gm"
+    args = ["convert" | args ++ [path]]
+    opts = [stderr_to_stdout: true]
+    case System.cmd(command, args, opts) do
+      {_, 0}     -> :ok
+      {error, _} -> {:error, error}
+    end
+  end
 end
--- test/gm_test.exs
+++ test/gm_test.exs
@@ -5,4 +5,12 @@ defmodule GmTest do
   test "open/1" do
     assert Gm.open("test.jpg") == %Gm.Command{args: ["test.jpg"]}
   end
+
+  test "write/2" do
+    assert :ok = Gm.open("xc:red") |> Gm.write("_build/test.jpg")
+
+    assert {:error, error} = Gm.open("test.jpg") |> Gm.write("_build/test.jpg")
+    assert error =~ "Unable to open file"
+    assert error =~ "test.jpg"
+  end
 end
